ec9ecdd3a851a94de28f2637b9f71534b5793648
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2016-08-03 23:19:28 -0400

    Get random word from dictionary

--- game.rb
+++ game.rb
@@ -3,9 +3,17 @@ require './hangman'
 include Hangman
 
 def next_guess game
+    guess = gets
+    if guess.nil?
+        game.force_quit
+    else
+        guess.chomp!
+    end
+
+    puts guess
 end
 
-game = Game.new
+game = Game.new '5desk.txt'
 
 loop do
     break if game.status != :continue
--- hangman.rb
+++ hangman.rb
@@ -1,10 +1,50 @@
 module Hangman
     class Game
-        attr_reader :status
+        attr_reader :status, :guessed, :revealed
+
+        def initialize(dictionary)
+            raise DictionaryNotFound.new unless File.exists? dictionary
+            @word = get_word dictionary
+            @revealed = Array.new(@word.length)
+            @letters_left = @word.length
+            @guessed = {}
+
+            puts @word
+
+            @status = :continue
+        end
+
+        def force_quit
+            @status = :ended
+        end
+
+        private
+
+        def get_word dictionary
+            words = File.open(dictionary, 'r').readlines
+
+            words.select! do |word| 
+                word.chomp!
+                word.length >= 5 && word.length <= 12
+            end
+
+            word = words.sample
+            if word.nil?
+                raise EmptyDictionary.new
+            else
+                word
+            end
+        end
     end
 
     class Display
-        def print game
+        def self.print game
         end
+    end
+
+    class DictionaryNotFound < IOError
+    end
+
+    class EmptyDictionary < StandardError
     end
 end
