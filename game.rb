a281f89aebd1d784d2ee1e78824e3035bdba9156
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2016-08-05 16:53:05 -0400

    Update game loop
    The main loop now uses catch-throw to break out when the game has ended.

--- game.rb
+++ game.rb
@@ -13,14 +13,27 @@ def next_guess game
     puts guess
 end
 
+def handle_status status
+    case status
+    when :ended
+        throw :break, status
+    when :good_guess, :bad_guess
+        puts Game.board.to_s
+    when :invalid_guess
+        puts "Already guessed that letter. Try again."
+    end
+end
+
 game = Game.new '5desk.txt'
 
-loop do
-    break if game.status != :continue
+result = catch(:break) do
+    loop do
+        handle_status game.status
 
-    Display.print(game)
+        print "Guess a letter: "
 
-    print "Guess a letter: "
-
-    next_guess game
+        next_guess game
+    end
 end
+
+puts "Game #{result}"
